05e5b9e66c77a28e323cb40f9ef19ef4a654f623
Elliot Kempson <kempsone@cardiff.ac.uk>
2022-01-25 15:00:34 +0000

    Walking Mario

--- game.py
+++ game.py
@@ -1,4 +1,5 @@
 import win32gui, win32con, curses, sys, time
+from pynput.keyboard import Key, Listener
 CURSES_COLORS = [curses.COLOR_WHITE, curses.COLOR_BLACK, curses.COLOR_RED, curses.COLOR_BLUE, curses.COLOR_GREEN, curses.COLOR_MAGENTA, curses.COLOR_CYAN, curses.COLOR_YELLOW]
 FRAMERATE = 60
 KEY_LEFT = "a"
@@ -25,7 +26,7 @@ mario_sprite = [[9,9,9,9,9,3,3,3,3,3,3,9,9,9,9,9,9],
                 [9,9,2,2,2,2,2,9,9,9,2,2,2,2,2,9,9]]
 
 mario_walking = [
-                [[9,9,9,9,9,9,9,9,9,9,9,9,9,9,9,9,9],
+                [[9,9,9,9,9,9,9,9,9,9,9,9,9,9,9,9],
                  [9,9,9,9,9,3,3,3,3,3,9,9,9,9,9,9],
                  [9,9,9,9,3,3,3,3,3,3,3,3,3,9,9,9],
                  [9,9,9,9,2,2,2,8,8,2,8,9,9,9,9,9],
@@ -59,7 +60,7 @@ mario_walking = [
                  [9,9,2,9,9,9,9,2,2,2,9,9,9,9,9,9],
                  [9,9,9,9,9,9,9,9,2,2,2,9,9,9,9,9]],
                  
-                 [[9,9,9,9,9,9,9,9,9,9,9,9,9,9,9,9,9],
+                 [[9,9,9,9,9,9,9,9,9,9,9,9,9,9,9,9],
                  [9,9,9,9,9,3,3,3,3,3,9,9,9,9,9,9],
                  [9,9,9,9,3,3,3,3,3,3,3,3,3,9,9,9],
                  [9,9,9,9,2,2,2,8,8,2,8,9,9,9,9,9],
@@ -91,25 +92,7 @@ mario_walking = [
                  [9,9,3,3,3,3,3,3,3,3,3,3,3,2,2,9],
                  [9,2,2,3,3,3,9,9,9,9,3,3,3,2,2,9],
                  [9,2,2,2,9,9,9,9,9,9,9,9,9,9,9,9],
-                 [9,9,2,2,2,9,9,9,9,9,9,9,9,9,9,9]],
-                 
-                 [[9,9,9,9,9,9,9,9,9,9,9,9,9,9,9,9,9],
-                 [9,9,9,9,9,3,3,3,3,3,9,9,9,9,9,9],
-                 [9,9,9,9,3,3,3,3,3,3,3,3,3,9,9,9],
-                 [9,9,9,9,2,2,2,8,8,2,8,9,9,9,9,9],
-                 [9,9,9,2,8,2,8,8,8,2,8,8,8,9,9,9],
-                 [9,9,9,2,8,2,2,8,8,8,2,8,8,8,9,9],
-                 [9,9,9,2,2,8,8,8,8,2,2,2,2,9,9,9],
-                 [9,9,9,9,9,8,8,8,8,8,8,8,9,9,9,9],
-                 [9,9,9,9,2,2,2,3,2,2,9,9,9,9,9,9],
-                 [9,9,9,2,2,2,2,3,3,2,2,9,9,9,9,9],
-                 [9,9,9,2,2,2,3,3,8,3,3,9,9,9,9,9],
-                 [9,9,9,2,2,2,2,3,3,3,3,9,9,9,9,9],
-                 [9,9,9,3,2,2,8,8,3,3,3,9,9,9,9,9],
-                 [9,9,9,9,3,2,8,8,3,3,9,9,9,9,9,9],
-                 [9,9,9,9,9,3,3,3,2,2,2,9,9,9,9,9],
-                 [9,9,9,9,9,2,2,2,2,9,9,9,9,9,9,9]],
-                 
+                 [9,9,2,2,2,9,9,9,9,9,9,9,9,9,9,9]]
                  ]
 
 brick_sprite = [[8,1,1,1,1,1,1,1,1,2,8,1,1,1,1,8],
@@ -146,7 +129,7 @@ class Sprite:
         self.height = len(pixel_array)
         self.center = (0, 0)    # Top Left
         self.animations = {}
-        self.animations["default"] = pixel_array
+        self.animations["default"] = [pixel_array]
         self.current_animation = "default"
         self.current_frame = 0
     
@@ -166,6 +149,7 @@ class Sprite:
         if animation in self.animations:
             self.current_frame = 0
             self.current_animation = animation
+            self.pixel_array = self.animations[self.current_animation][0]
             
     def next_frame(self):
         if len(self.animations[self.current_animation]) - 1 == self.current_frame:
@@ -192,6 +176,7 @@ class Mario(GameObject):
         self.x_speed = 0x0000
         self.y_speed = 0  
         self.direction = "R"
+        self.moving = False
         self.__max_walk_speed = 0x1900
         self.__max_sprint_speed = 0x2900
         
@@ -210,6 +195,27 @@ class Block(GameObject):
     def __init__(self, sprite, x_pos, y_pos):
         super().__init__(sprite, x_pos, y_pos)
         
+def flip_animation(animation):
+    flipped_animation = []
+    for frame in animation:
+        new_frame = []
+        for x, line in enumerate(frame):
+            new_frame.append(line[::-1])
+            
+        flipped_animation.append(new_frame)
+        
+    return flipped_animation
+        
+def list_remove(list, obj):
+    try:
+        new_list = []
+        for item in list:
+            new_list.append(item)
+        new_list.remove(obj)
+        return new_list
+    except ValueError:
+        return list
+        
 
 def play_game():
     def _play_game(stdscr):
@@ -227,25 +233,44 @@ def play_game():
         key_down = "s"
         key_jump = " "
         key_sprint = "\\"
-        
-        
         bg_color = 7
         surf = Surface(background_color=bg_color)
         player = Mario(mario_sprite, 25, 34)
-        player.sprite.add_animation(mario_walking, "walking")
+        player.sprite.add_animation(mario_walking, "walkingR")
+        player.sprite.add_animation(flip_animation(mario_walking), "walkingL")
         bricks = [Block(brick_sprite, x, 50) for x in range(0, 256, 16)]
         playing = True
         frame = 0
         code=0
+        code2= 0
         code_list = []
+        t_list = []
+        prev_frame = 0
+        prev_case = -1
+        locked = False
+        test_list = [1, 5, 6, 7, 8, 9]
+        test_list2 = list_remove(test_list, 8)
 
         prev_time = time.perf_counter()
         while playing:
+            debug_messages = [f"{time.perf_counter()}",
+                              f"{frame=}", 
+                              f"{hex(player.x_speed)=}",
+                              f"{player.x_pos=}",
+                              f"{player.direction=}",
+                              f"{code=}",
+                              f"{code2=}",
+                              f"{code_list=}",
+                              f"{all(c == -1 for c in t_list)}",
+                              f"{locked=}",
+                              f"{prev_frame=}",
+                              f"{prev_case=}"
+                              ]
             # Limit FPS to FRAMERATE
             if time.perf_counter() - prev_time >= 1/FRAMERATE:
                 prev_time = time.perf_counter()
-                if frame == 60:
-                    frame = 0
+                #if frame == 60:
+                 #   frame = 0
                 frame += 1
                 
                 
@@ -254,7 +279,17 @@ def play_game():
                     
                 ## USER INPUT
                 code = stdscr.getch()
+                code2= stdscr.getch()
+                code3 = stdscr.getch()
                 code_list.append(code)
+                
+                    
+                if locked:
+                    if all(c == -1 for c in t_list) and frame < prev_frame+25:
+                        code = prev_case
+                    elif not all(c== -1 for c in t_list) or frame >= prev_frame+25:
+                        locked = False
+                        
                 if code != -1:
                     match chr(code):
                         case "d":
@@ -265,12 +300,18 @@ def play_game():
                                     player.accelerate(0x0098, False, False)
                             else:
                                 player.accelerate(0x0098, False, True)
-                            
-                            player.direction = "R"
-                            if player.sprite.current_animation != "walking":
-                                player.sprite.change_animation("walking")
                                 
-                        
+                            if all(c == -1 for c in list_remove(code_list, ord("d"))) and not locked:
+                                locked = True
+                                prev_frame = frame
+
+                            prev_case = ord("d")
+                            player.direction = "R"
+                            if player.x_speed >= 0x1000:
+                                player.moving = True
+                            if player.sprite.current_animation != "walkingR" and player.x_speed >= 0x1000:
+                                player.sprite.change_animation("walkingR")
+                                
                         case "a":
                             if player.direction == "L":
                                 if player.x_speed > -0x0630:
@@ -280,9 +321,18 @@ def play_game():
                             else:
                                 player.accelerate(-0x0098, False, True)
                                 
+                            if all(c == -1 for c in list_remove(code_list, ord("a"))) and not locked:
+                                locked = True
+                                prev_frame = frame
+                                
+                            prev_case = ord("a")
                             player.direction = "L"
+                            if player.x_speed <= -0x1000:
+                                player.moving = True
+                            if player.sprite.current_animation != "walkingL" and player.x_speed <= -0x1000:
+                                player.sprite.change_animation("walkingL")
                         
-                if all(code == -1 for code in code_list):
+                elif all(c == -1 for c in code_list) and not locked:
                     if player.direction == "R":
                         player.accelerate(-0x00D0, False, False)
                         if player.x_speed < 0:
@@ -291,18 +341,23 @@ def play_game():
                         player.accelerate(0x00D0, False, False)
                         if player.x_speed > 0:
                             player.x_speed = 0
-                        
-                    player.sprite.change_animation("idle")    
+                    player.sprite.change_animation("default") 
+                    prev_case = -1   
                 
-                else:
-                    if frame % 15 == 0:
+                            
+                if player.x_speed <= -0x1000 or player.x_speed >= 0x1000:
+                                player.moving = True
+                else: 
+                    player.moving = False
+                        
+                    
+                
+                if player.moving and frame % 10 == 0:
+                    if player.sprite.current_animation in ("walkingL", "walkingR"):
                         player.sprite.next_frame()
                      
                 player.update_pos()
                 stdscr.refresh()
-                    
-
-                
                 
                 ## PLACE SPRITES
                 surf.grid = [[7 for _ in range(230)] for _ in range(66)]
@@ -317,17 +372,10 @@ def play_game():
                             except curses.error:
                                 pass
                             
-                ## Info and debug
-                
+            ## Info and debug
             try:
-                stdscr.addstr(0, 0, f"{time.perf_counter()}")
-                stdscr.addstr(1, 0, f"{frame=}")
-                stdscr.addstr(2, 0, f"{hex(player.x_speed)=}")
-                stdscr.addstr(3, 0, f"{player.x_pos=}")
-                stdscr.addstr(4, 0, f"{player.direction=}")
-                stdscr.addstr(5, 0, f"{code}")
-                stdscr.addstr(6, 0, f"{all(c == -1 for c in code_list)}")
-                
+                for n, message in enumerate(debug_messages):
+                    stdscr.addstr(n, 0, message)
                 
             except curses.error:
                 pass
